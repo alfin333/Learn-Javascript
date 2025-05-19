5ef8ed7cadb295b3a3d74c5298fbf96369e19af0
alfin333 <alfin2501@gmail.com>
2025-05-19 19:41:22 +0700

    Menambahkan bahan pembelajaran untuk mempelajari method array for each sebagai bahan pembelajaran ke 40

---
+++ JAVASCRIPT PROJECT/31-40/40. Belajar Array for Each/40arrayFEach.js
@@ -0,0 +1,15 @@
+//array.forEach() = executes a provided callback function
+//                  once for each array element
+
+let students = ["alfin", "gilang", "adit"];
+students.forEach(capitalize)
+students.forEach(print)
+
+function capitalize(element, index, array){
+    array[index] = element[0].toUpperCase() + element.substring(1);
+    // kalian bisa coba ubah argumen substring untuk memahami cara kerjanya
+    // intinya ini agar huruf pertama kapital
+}
+function print(element){
+    console.log("ini function print: ",element);
+}
