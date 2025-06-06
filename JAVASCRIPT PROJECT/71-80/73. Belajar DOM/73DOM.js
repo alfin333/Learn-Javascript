da7b2a2af164f0b8bdced247f22560dfa01c8114
alfin333 <alfin2501@gmail.com>
2025-06-06 20:45:57 +0700

    Menambahkan bahan pembelajaran DOM sebagai bahan pembelajaran ke 73

---
+++ JAVASCRIPT PROJECT/71-80/73. Belajar DOM/73DOM.js
@@ -0,0 +1,13 @@
+// DOM =    Document Object Model (API)
+//          An Interference for changing the content of a page
+
+//console.log(document);
+console.dir(document); // kasih liat properti DOM kamu
+//console.log(document.title);
+//console.log(document.URL);
+//document.title = "Title goes here!";
+//document.location = "http://www.google.com";
+//document.body.style.backgroundColor = "skyblue";
+//document.getElementById("myDiv").innerHTML = "<h1> Hello";
+
+// intinya untuk mengubah element gitu 
