805959cb3ef43fe3275e6398d51cf432ea7e30d4
alfin333 <alfin2501@gmail.com>
2025-06-09 17:47:48 +0700

    Menambahkan bahan pembelajaran Change CSS Properties sebagai bahan pembelajaran ke 76

---
+++ JAVASCRIPT PROJECT/71-80/76. Belajar Change CSS Properties/76changeCSSP.js
@@ -0,0 +1,12 @@
+const title = document.getElementById('myTitle');
+// title.style.backgroundColor = "rgb(50, 200, 150)";
+title.style.backgroundColor = "#222222";
+title.style.color = "rgb(50, 200, 250)";
+title.style.fontFamily ="consolas";
+// titleS = title.style;
+title.style.textAlign = "center"
+title.style.border = "10px solid orange";
+title.style.padding = "10px";
+title.style.display = "block" // coba ganti jadi "none"
+
+// penulisan ini kalau di javascript harus camelCase 
