565459725e62531865fe5932bc7e430a38c300b9
alfin333 <alfin2501@gmail.com>
2025-06-09 17:20:12 +0700

    Menambahkan bahan pembelajaran DOM Traversal sebagai bahan pembelajaran ke 75

---
+++ JAVASCRIPT PROJECT/71-80/75. Belajar DOM Traversal/75DOMtravers.js
@@ -0,0 +1,27 @@
+let element = document.querySelector("#dessert");
+console.log(element)
+
+let child = element.previousElementSibling;
+child.style.backgroundColor = "lightgreen"
+
+let elements = document.querySelector("#fruit");
+console.log(elements)
+
+let children = Array.from(elements.children);
+children.forEach(child => {
+    child.style.backgroundColor = "pink"
+})
+
+//.firstElementChild
+//.lastElementChild
+//.parentElement
+//.nextElementSibling
+//.previousElementSibling
+//.children[]
+//Array.from(.children)
+
+//KESIMPULAN: kalian bisa coba coba sendiri querynya, 
+// jika kita milih #fruits dan pilih firstElemenChild
+// otomatis dia akan cari child elemen pada id fruits, dalam hal ini
+// child elementnya adalah si <li>banana</li>
+// kalau kita pilih document.body, lastElementChild nya kemungkinan si <script>
